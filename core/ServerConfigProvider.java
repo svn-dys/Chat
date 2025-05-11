f6f050494e38cfcd651afd699edca1e2f36c7563
svn-dys <11863033+svn-dys@users.noreply.github.com>
2025-05-11 19:27:08 -0400

    Update ServerConfigProvider.java

--- core/ServerConfigProvider.java
+++ core/ServerConfigProvider.java
@@ -4,7 +4,7 @@ package core;
  * Provides a single ServerConfig instance. Cannot be reinstantiated after initialization.
  * The main idea behind a ServerConfigProvider is to have a single source of truth for the server configuration.
  * This way, the server configuration is highly decoupled from the rest of the application. Along with this,
- * the clients/{@link ui.UINetworkThread}UI-Network-Thread know of the server configuration
+ * the clients/UI-Network-Thread know of the server configuration
  * and can use it to connect to the server through its associated UI network sockets.
 */
 public final class ServerConfigProvider {
