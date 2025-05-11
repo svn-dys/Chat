8f0ebf962afa87e6c93b35f391f4ea03d3837837
svn-dys <11863033+svn-dys@users.noreply.github.com>
2025-05-11 18:59:34 -0400

    Update comment in ChatTest.java

--- testing/ChatTest.java
+++ testing/ChatTest.java
@@ -12,14 +12,16 @@ import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
 /*
-    [Instructions & usage guide]:
-        1: Compile Java files from inside /src/ folder:
-            javac -d out core\*.java server\*.java ui\*.java testing\*.java
-        2: This command starts the chat server and 2 chat windows:
-            java -ea -cp out testing.ChatTest --windowamount 2
-    [Runtime args]:
-        --noserver: Don't start a server, create X number of chat windows
-        --windowamount: Specify the number of chat windows to create
+  * Example instructions & usage guide:
+  *   1: Compile Java files from inside /src/ folder:
+  *       javac -d out core\*.java server\*.java ui\*.java testing\*.java
+  *   - This command starts the chat server and 2 chat windows:
+  *       java -ea -cp out testing.ChatTest --windowamount 2 
+  *   - This command starts just a single window and no server:
+  *       java -ea -cp out testing.ChatTest --noserver --windowamount 1
+  * Runtime args:
+  *   [--noserver]: Don't start a server.
+  *   [--windowamount]: Number of chat windows to create.
 */
 public final class ChatTest {
     private static final int PORT = 1337;
